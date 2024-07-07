f1c873a50aa5d72d6ceaea05519550b28b42c6cc
AngelTravieso <angeltraviesoc@gmail.com>
2024-07-07 15:15:11 -0400

    Token del dispositivo y determinar permiso actual

--- lib/config/presentation/blocs/notifications/notifications_bloc.dart
+++ lib/config/presentation/blocs/notifications/notifications_bloc.dart
@@ -12,6 +12,12 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
 
   NotificationsBloc() : super(const NotificationsState()) {
     on<NotificationsStatusChanged>(_notificationsStatusChanged);
+
+    // Ejecutar metodos en el contructor (es parecido a cuando lo llamo en el constructor en riverpod)
+    // Apenas la aplicacion se llama lanzar el prompt para los permisos
+    // requestPermission();
+
+    _initialStatusCheck();
   }
 
   static Future<void> initializeFCM() async {
@@ -25,9 +31,44 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
     emit(state.copyWith(
       status: event.status,
     ));
+
+    // Obtener el token
+    _getFCMToken();
+  }
+
+  // Con esto verifico el status de los permisos para notificaciones
+  void _initialStatusCheck() async {
+    // Determinar el estado actual
+    final settings = await messaging.getNotificationSettings();
+
+    // El cambio del status es sincrono
+    add(NotificationsStatusChanged(settings.authorizationStatus));
+
+    // Obtener el token
+    // _getFCMToken();
+
+    // Se tiene un token especifico, esto es para enviar a un dispositivo/usuario en especifico
+    /**
+     * un usuario puede tener varios dispostivos
+     * angeltraviesoc@gmail.com {
+     * token1
+     * token2
+     * token3
+     * }
+     */
+  }
+
+  // Esto obtiene el token unico del dispositivo
+  void _getFCMToken() async {
+    if (state.status != AuthorizationStatus.authorized) return;
+
+    final token = await messaging.getToken();
+
+    print(token);
   }
 
   void requestPermission() async {
+    // Aca pido el permiso
     NotificationSettings settings = await messaging.requestPermission(
       alert: true,
       announcement: false,
@@ -39,5 +80,7 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
     );
 
     add(NotificationsStatusChanged(settings.authorizationStatus));
+    // Obtener el token
+    // _getFCMToken();
   }
 }
